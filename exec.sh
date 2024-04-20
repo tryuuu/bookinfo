d9310e6f4900ff38ccba097325c4cbe608049ceb
tryuhowcollect <t.ryu.howcollect@gmail.com>
2024-04-20 19:15:04 +0900

    #2

--- exec.sh
+++ exec.sh
@@ -5,8 +5,7 @@ kubectl label namespace default istio-injection=enabled
 kubectl apply -f samples/bookinfo/platform/kube/bookinfo.yaml
 kubectl apply -f samples/bookinfo/networking/bookinfo-gateway.yaml
 kubectl apply -f samples/bookinfo/networking/destination-rule-all.yaml
-kubectl apply -f samples/bookinfo/networking/virtual-service-all-v1.yaml
-
+kubectl apply -f samples/bookinfo/networking/virtual-service-reviews-test-v2.yaml
 
 kubectl get services
 minikube service productpage --url
