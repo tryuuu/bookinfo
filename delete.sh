48f6c1be3e44659175b57cff6a92058558418d04
tryuhowcollect <t.ryu.howcollect@gmail.com>
2024-04-20 18:56:15 +0900

    #1

--- delete.sh
+++ delete.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 kubectl delete -f samples/bookinfo/platform/kube/bookinfo.yaml
 kubectl delete -f samples/bookinfo/networking/bookinfo-gateway.yaml
+kubectl delete -f samples/bookinfo/networking/destination-rule-all.yaml
+kubectl delete -f samples/bookinfo/networking/virtual-service-all-v1.yaml
