1313334b199f986ae2417da79ef417d282799d8e
tryuhowcollect <t.ryu.howcollect@gmail.com>
2024-04-21 20:42:58 +0900

    #7

--- exec.sh
+++ exec.sh
@@ -4,12 +4,17 @@ kubectl label namespace default istio-injection=enabled
 
 kubectl apply -f samples/bookinfo/platform/kube/bookinfo.yaml
 kubectl apply -f samples/bookinfo/networking/bookinfo-gateway.yaml
-kubectl apply -f samples/bookinfo/networking/destination-rule-all.yaml
-kubectl apply -f samples/bookinfo/networking/reviews-timeout.yaml
+kubectl apply -f samples/bookinfo/networking/httpbin-destinationrule.yaml
+kubectl apply -f https://raw.githubusercontent.com/istio/istio/master/samples/httpbin/httpbin.yaml
+kubectl apply -f https://raw.githubusercontent.com/istio/istio/master/samples/httpbin/sample-client/fortio-deploy.yaml
 
-kubectl get services
-echo ""
-kubectl get pods
-minikube service productpage --url
+# Fortioポッドの起動を待機
+echo "Waiting for Fortio pod to be ready..."
+kubectl wait --for=condition=ready pod -l app=fortio --timeout=120s
+# Fortioポッド名を取得して負荷テストを実行
+FORTIO_POD=$(kubectl get pods -l app=fortio -o 'jsonpath={.items[0].metadata.name}')
+kubectl exec "$FORTIO_POD" -c fortio -- /usr/bin/fortio load -c 2 -qps 0 -n 20 -loglevel Warning http://httpbin:8000/get
+
+
 
 
