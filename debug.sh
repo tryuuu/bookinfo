27a0163f1e11a491bc7c8961ef94b65021a0aa84
tryuhowcollect <t.ryu.howcollect@gmail.com>
2024-04-21 17:49:07 +0900

    #5

--- debug.sh
+++ debug.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 istioctl analyze
 kubectl get destinationrules -o yaml
+kubectl get virtualservice ratings -o yaml
+istioctl proxy-status
+kubectl get svc,pods -l app=ratings
--- exec.sh
+++ exec.sh
@@ -5,9 +5,7 @@ kubectl label namespace default istio-injection=enabled
 kubectl apply -f samples/bookinfo/platform/kube/bookinfo.yaml
 kubectl apply -f samples/bookinfo/networking/bookinfo-gateway.yaml
 kubectl apply -f samples/bookinfo/networking/destination-rule-all.yaml
-kubectl apply -f samples/bookinfo/networking/virtual-service-all-v1.yaml
-kubectl apply -f samples/bookinfo/networking/virtual-service-reviews-test-v2.yaml
-kubectl apply -f samples/bookinfo/networking/virtual-service-ratings-test-delay.yaml
+kubectl apply -f samples/bookinfo/networking/virtual-service-ratings-test-abort.yaml
 
 kubectl get services
 echo ""
